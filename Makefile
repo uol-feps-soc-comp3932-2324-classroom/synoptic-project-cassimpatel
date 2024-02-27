29dd5cd2f64a56bddf51ca215fdb7c2370620434
Cassim Patel <cassim.y.patel@pwc.com>
2024-02-27 21:56:52 +0000

    minor updates to package info, makefile comments

--- Makefile
+++ Makefile
@@ -1,1 +1,2 @@
 # TODO: add optional targets for e.g. setting up and installation, running tests, cleanup etc.
+# examples: init (install requirements), make venv, install custom packages, run tests
--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 import setuptools
 
-setuptools.setup(
-    name="src",
-    version="0.0.1",
-    description="A highly customisable version of Spectral Clustering and Data Generation",
-    packages=setuptools.find_packages(),
-    python_requires='>=3.7',
+setuptools.setup (
+    name            = "src",
+    version         = "0.0.1",
+    description     = "A highly customisable version of Spectral Clustering and Data Generation",
+    author          = "Cassim Patel",
+    author_email    = "sc20cp@leeds.ac.uk",
+    packages        = setuptools.find_packages(),
+    python_requires = '>=3.7',
+    install_requires=[
+        # TODO: add all packages that yours depends on
+        # 'markdown',
+    ],
 )
