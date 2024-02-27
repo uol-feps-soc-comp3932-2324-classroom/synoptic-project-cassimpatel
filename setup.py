ee4f6323cb803d52adea6417a5828806a606555b
Cassim Patel <cassim.y.patel@pwc.com>
2024-02-27 18:01:56 +0000

    Added package installation and instantiation

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="src",
+    version="0.0.1",
+    description="A highly customisable version of Spectral Clustering and Data Generation",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7',
+)
