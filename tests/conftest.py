3f5bbb3bf3c666831d9af5e6af8b49a60fa1f924
Cassim Patel <cassim.y.patel@pwc.com>
2024-03-04 20:32:02 +0000

    Changed dump to json, added more metrics

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,12 @@
 import os
 import time
-import pytest
+import json
 import signal
 import datetime
 from importlib import reload
+
 import src
- 
+from sklearn.metrics import cluster
 from src.data_generation import sklearn_make_moons
 
 
@@ -22,8 +23,8 @@ INPUT_NUM_MOONS = [3, 4, 5, 6]
 
 # where to store current run results
 RESULTS_DUMP_FOLDER     = f'./results/res_{datetime.datetime.now().strftime("%Y_%m_%d_T%H_%M_%S")}'
-RESULTS_TIMING_DOC      = f'{RESULTS_DUMP_FOLDER}/results_times.csv'
-RESULTS_CORRECTNESS_DOC = f'{RESULTS_DUMP_FOLDER}/results_correctness.csv'
+RESULTS_TIMING_DOC      = f'{RESULTS_DUMP_FOLDER}/results_times.json'
+RESULTS_CORRECTNESS_DOC = f'{RESULTS_DUMP_FOLDER}/results_correctness.json'
 RESULTS_REPORT_DOC      = f'{RESULTS_DUMP_FOLDER}/report.html'
 
 # complete set of modules available to test for SpectralClustering
@@ -40,10 +41,10 @@ def pytest_configure(config):
     os.makedirs(os.path.dirname(RESULTS_CORRECTNESS_DOC), exist_ok=True)
 
     # create empty files for both
-    with open(RESULTS_TIMING_DOC,'w') as f:
-        f.write('n_points,noise,variant,method,time\n')
-    with open(RESULTS_CORRECTNESS_DOC,'w') as f:
-        f.write('n_points,noise,variant,method,adjusted_rand_index\n')
+    with open(RESULTS_TIMING_DOC, 'w') as f:
+        f.write('')
+    with open(RESULTS_CORRECTNESS_DOC, 'w') as f:
+        f.write('')
 
     # set creation of HTML report
     config.option.htmlpath = f'{RESULTS_DUMP_FOLDER}/report.html'
@@ -77,11 +78,44 @@ def binary_moons_data(n_points, noise):
     return X, labels
 
 # helper functions: dumping new timing results, correctness etc.
-def dump_time(n_points, noise, time, variant = 'DEFAULT', method = 'DEFAULT'):
-    with open(RESULTS_TIMING_DOC,'a') as f:
-        new_line = f'{n_points},{noise},{variant},{method},{time}\n'
-        f.write(new_line)
-def dump_correctness(n_points, noise, adj_rand_index, variant = 'DEFAULT', method = 'DEFAULT'):
-    with open(RESULTS_CORRECTNESS_DOC,'a') as f:
-        new_line = f'{n_points},{noise},{variant},{method},{adj_rand_index}\n'
-        f.write(new_line)
+def dump_time(n_points, noise, time, experiment = 'DEFAULT', variant = 'DEFAULT'):
+    with open(RESULTS_TIMING_DOC, 'a') as f:
+        new_entry = {
+            'n_points'  : n_points,
+            'noise'     : noise,
+            'experiment': experiment,
+            'variant'   : variant,
+            'time'      : time,
+            'timed_out' : "True" if time == MAX_TIMEOUT_SECS else "False"
+        }
+        entry_json = json.dumps(new_entry, sort_keys=True)
+        f.write(f'{entry_json}\n')
+
+# calculate a set of metrics for correctness
+def calc_correctness(pred_labels, ground_truth):
+    # TODO: add more measures of correctness
+    return {
+        'adjusted_rand_score'         : cluster.adjusted_rand_score(ground_truth, pred_labels),
+        'adjusted_mutual_info_score'  : cluster.adjusted_mutual_info_score(ground_truth, pred_labels),
+        'normalized_mutual_info_score': cluster.normalized_mutual_info_score(ground_truth, pred_labels),
+        'homogeneity_score'           : cluster.homogeneity_score(ground_truth, pred_labels),
+        'completeness_score'          : cluster.completeness_score(ground_truth, pred_labels),
+        'v_measure_score'             : cluster.v_measure_score(ground_truth, pred_labels),
+    }
+
+def dump_correctness(n_points, noise, timed_out = False, pred_labels = None, ground_truth = None, experiment = 'DEFAULT', variant = 'DEFAULT'):
+    new_entry = {
+        'n_points'  : n_points,
+        'noise'     : noise,
+        'experiment': experiment,
+        'variant'   : variant,
+        'timed_out' : "True" if timed_out else "False"
+    }
+
+    if not timed_out:
+        metrics = calc_correctness(pred_labels, ground_truth)
+        new_entry.update(metrics)
+
+    with open(RESULTS_CORRECTNESS_DOC, 'a') as f:
+        entry_json = json.dumps(new_entry, sort_keys=True)
+        f.write(f'{entry_json}\n')
--- tests/test_correctness.py
+++ tests/test_correctness.py
@@ -1,18 +1,9 @@
 import pytest
 from src.SpectralClustering import SpectralClustering
-from sklearn.metrics.cluster import adjusted_rand_score
 from conftest import (
     NUM_REPEATS, MAX_TIMEOUT_SECS, INPUT_SIZES, INPUT_NOISES,
     binary_moons_data, dump_correctness, run_timeout_fn,
 )
-
-# calculate a set of metrics for correctness
-def calc_correctness(pred_labels, ground_truth):
-    # TODO: add more measures of correctness
-    return {
-        'adjusted_rand_score': adjusted_rand_score(ground_truth, pred_labels),
-    }
-
 
 @pytest.mark.parametrize('repeats'  , range(NUM_REPEATS))
 @pytest.mark.parametrize('n_points' , INPUT_SIZES)
@@ -24,8 +15,7 @@ def test_correctness_default(repeats, n_points, noise):
     t, y_pred  = run_timeout_fn(model.fit, X)
 
     if t == MAX_TIMEOUT_SECS:
-        dump_correctness(n_points, noise, 'TIMED_OUT')
+        dump_correctness(n_points, noise, timed_out = True)
         pytest.skip('Timed out')
 
-    metrics = calc_correctness(y_pred, y_true)
-    dump_correctness(n_points, noise, metrics['adjusted_rand_score'])
+    dump_correctness(n_points, noise, pred_labels = y_pred, ground_truth = y_true)
